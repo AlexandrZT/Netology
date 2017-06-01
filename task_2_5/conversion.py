273af3d9ee20b1cc538cd88a5e1c0b17d21ffb96
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-01 22:37:46 +0600

    task_2_5
    Done 2.5

---
+++ task_2_5/conversion.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+import glob
+import multiprocessing
+
+
+def convert(binary, image_name, dst_folder):
+    dst_file = os.path.join(dst_folder,os.path.basename(image_name))
+    subprocess.run([binary, image_name, '-resize', '200', dst_file])
+
+def locate_files(folder):
+    return glob.glob(os.path.join(folder,'*.jpg'))
+
+
+def prepare_folder(folder):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+def convert_worker(binary, image_name, dst_folder):
+    print(binary, image_name, dst_folder)
+
+def convert_images(converter, dst_file):
+    #thread_count = input('Запустить 1 поток(s) или 4ре(m)?:')
+    folder_name = 'Source'
+    convert_files = locate_files(folder_name)
+    thread_count = '4'
+    prepare_folder(dst_file)
+    if thread_count.lower() == 's':
+        for img in convert_files:
+            convert(converter, convert_files[0], dst_file)
+    elif:
+        wprocess = []
+        for pnom in range(int(thread_count)):
+            mproc = multiprocessing.Process(target=convert, args=(converter, convert_files[pnom], dst_file))
+            wprocess.append(mproc)
+            mproc.start()
+        for wproc in wprocess:
+            wproc.join()
+
+
+
+if __name__ ==  '__main__':
+    converter_bin = 'convert.exe'
+    destination_folder = 'Destination'
+    convert_images(converter_bin, destination_folder)
