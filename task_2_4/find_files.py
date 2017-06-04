6767916a9f1e87ed181eb5fcf6b60ae3d280ec3d
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-04 23:22:34 +0600

    2_4 fix
    Исправлены замечения по дз 2.4
    удалён отладочный код, созданы функции

--- task_2_4/find_files.py
+++ task_2_4/find_files.py
@@ -1,19 +1,28 @@
 import glob
 import os.path
 
+
 migrations = 'Migrations'
 
-files = glob.glob(os.path.join(migrations, "*.sql"))
-print(type(files))
-file_list_iter = files
-while True:
-    search_string = input('Введите строку для поиска(!q - для выхода): ')
-    if search_string == '!q':
-        break
-    file_list = []
-    for file in file_list_iter:
-        if search_string in open(file).read():
-            file_list.append(file)
-            print(file)
-    print('{}'.format(len(file_list)))
-    file_list_iter = file_list
+
+def locate_files(folder):
+    files = glob.glob(os.path.join(folder, "*.sql"))
+    return files
+
+
+def search_data(search_at_files):
+    file_list_iter = search_at_files
+    while True:
+        search_string = input('Введите строку для поиска(!q - для выхода): ')
+        if search_string == '!q':
+            break
+        file_list = []
+        for file in file_list_iter:
+            if search_string in open(file).read():
+                file_list.append(file)
+                print(file)
+        print('{}'.format(len(file_list)))
+        file_list_iter = file_list
+
+look_files = locate_files(migrations)
+search_data(look_files)
