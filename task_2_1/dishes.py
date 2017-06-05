ef8051d182ae90e5a54026ad18f599b397ec65ed
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-05 10:26:32 +0600

    fix 2 1(1)
    Исправлены замечания

--- task_2_1/dishes.py
+++ task_2_1/dishes.py
@@ -2,27 +2,26 @@ import os
 
 def load_cook_book(file_name):
     loaded_cook_book = {}
-    recepie_cntr = 0
     read_file = True
     if os.path.exists(file_name):
         with open(file_name, 'r') as f:
             while read_file:
                 recepie_name = f.readline().lower().strip()
-                if recepie_name == '':
+                if not recepie_name:
                     read_file = False
                     continue
                 ingredients_num = int(f.readline())
                 loaded_cook_book[recepie_name] = []
-                recepie_cntr += 1
                 for item in range(ingredients_num):
                     ingredient = f.readline().lower()
                     list_ingedients = ingredient.split('|')
-                    loaded_cook_book[recepie_name].append({'ingridient_name': list_ingedients[0].strip(), \
-                                                       'quantity': int(list_ingedients[1].strip()), 'measure': list_ingedients[2].strip()})
+                    loaded_cook_book[recepie_name].append({'ingridient_name': list_ingedients[0].strip(),
+                                                           'quantity': int(list_ingedients[1].strip()),
+                                                           'measure': list_ingedients[2].strip()})
     else:
         print('Ошибка загрузки файла рецептов.')
         return False
-    print('Загружено {} рецепта(ов).'.format(recepie_cntr))
+    print('Загружено {} рецепта(ов).'.format(len(loaded_cook_book)))
     return loaded_cook_book
 
 def get_shop_list_by_dishes(dishes, person_count, cook_book):
@@ -51,6 +50,7 @@ def create_shop_list():
     else:
         wrk_cook_book = {}
         print("Не загружен файл рецептов. Выход.")
+        return
     person_count = int(input('Введите количество человек: '))
     dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
         .lower().split(', ')
