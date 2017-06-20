2d5f833fe71c5f248d800c42e8e86c94b9a88bbb
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-20 15:02:48 +0600

    task 3 5 task 2 done

--- task_3_5/cource.py
+++ task_3_5/cource.py
@@ -5,7 +5,6 @@ import os
 
 def load_task1(file_name):
     fstrings = []
-    print()
     URL = 'http://www.webservicex.net/ConvertTemperature.asmx?WSDL'
     if os.path.isfile(file_name):
         with open(file_name, 'r') as fhndl:
@@ -18,8 +17,28 @@ def load_task1(file_name):
     fromUnit = 'degreeFahrenheit'
     toUnit = 'degreeCelsius'
     srv_response = sclient.service.ConvertTemp(avg_temperature, fromUnit, toUnit)
-    print("{:.2f}\u00B0C".format(srv_response))
+    print("Средняя температура: {:.2f}\u00B0C".format(srv_response))
+
+def load_task2(file_name):
+    CONVERT_URL = 'http://fx.currencysystem.com/webservices/CurrencyServer4.asmx?WSDL'
+    sclient = osa.Client(CONVERT_URL)
+    # srv_response = sclient.service.ConvertToNum(fromCurrency = 'USD', toCurrency = 'RUB', amount = 100.0, rounding = True)
+    trip_cost = 0
+    if os.path.isfile(file_name):
+        with open(file_name, 'r') as fhndl:
+            for file_line in fhndl.readlines():
+                line_components = file_line.split(' ')
+                currency = line_components[2].strip()
+                read_amount = line_components[1]
+                srv_response = sclient.service.ConvertToNum(fromCurrency=currency, toCurrency='RUB',
+                                                            amount=read_amount, rounding=True)
+                trip_cost += round(srv_response, 0)
+    print('Стоимость путешевствия: {} руб.'.format(int(trip_cost)))
+
+def load_task3(file_name):
+    pass
 
 
 if __name__ == '__main__':
-    load_task1('temps.txt')
+    # load_task1('temps.txt')
+    load_task2('currencies.txt')
