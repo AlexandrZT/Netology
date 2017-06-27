2e881a04bcfc4953b7958150f0f6c869e216fd11
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-27 21:09:27 +0600

    task 3 6 new class structure

--- task_3_7/yaMetrics.py
+++ task_3_7/yaMetrics.py
@@ -1,2 +1,12 @@
 # --*--coding: utf-8 --*--
 import requests
+
+
+class YaMetrics(api_key, api_password):
+    API_KEY = ''
+    API_PASSWORD = ''
+
+    def __init__(self, api_key, api_password):
+        self.API_KEY = api_key
+        self.API_PASSWORD = api_password
+
