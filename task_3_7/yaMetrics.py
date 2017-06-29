21213c6663311cdd863538e03d0ced582aa88dcd
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-29 07:06:34 +0600

    task 3 7 initial commit

--- task_3_7/yaMetrics.py
+++ task_3_7/yaMetrics.py
@@ -1,8 +1,16 @@
-# --*--coding: utf-8 --*--
+# -*-coding: utf-8 -*-
 import requests
+import urllib
 
+def create_url(app_id):
+    URL = 'https://oauth.yandex.ru/authorize'
+    url_parametrs = {
+        'response_type' : 'token',
+        'client_id' : app_id
+        }
+    return '?'.join((URL, urllib.parse.urlencode(url_parametrs)))
 
-class YaMetrics(api_key, api_password):
+class YaBase():
     API_KEY = ''
     API_PASSWORD = ''
 
@@ -10,3 +18,10 @@ class YaMetrics(api_key, api_password):
         self.API_KEY = api_key
         self.API_PASSWORD = api_password
 
+
+
+
+class YaCounters(YaBase):
+    pass
+
+print(create_url(''))
