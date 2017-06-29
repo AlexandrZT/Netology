3ee8d1e70fea041447d4d6f58489cd68d678a6a8
Alexandr Zaburdyayev <alexandr_zft@mail.ru>
2017-06-29 23:05:21 +0600

    task 3 7 first working

--- task_3_7/yaMetrics.py
+++ task_3_7/yaMetrics.py
@@ -1,15 +1,16 @@
 # -*-coding: utf-8 -*-
 import requests
 import urllib
-import pprint
+
 
 def create_url(app_id):
-    URL = 'https://oauth.yandex.ru/authorize'
+    url = 'https://oauth.yandex.ru/authorize'
     url_parametrs = {
-        'response_type' : 'token',
-        'client_id' : app_id
+        'response_type': 'token',
+        'client_id': app_id
         }
-    return '?'.join((URL, urllib.parse.urlencode(url_parametrs)))
+    return '?'.join((url, urllib.parse.urlencode(url_parametrs)))
+
 
 class YaBase():
     API_KEY = ''
@@ -25,6 +26,7 @@ class YaBase():
             'Authorization': 'OAuth {}'.format(self.API_TOKEN),
             'Content-Type': 'application/json'
         }
+
     def check_run(self):
         if self.API_KEY and self.API_TOKEN:
             return True
@@ -40,8 +42,7 @@ class YaCounters(YaBase):
     def get_counter_id(self):
         if not self.check_run():
             return False
-        req_headers = self.prepare_headers()
-        response = requests.get( '/'.join((self.BASE_URL, self.COUNTERS_URL)), headers=req_headers )
+        response = requests.get('/'.join((self.BASE_URL, self.COUNTERS_URL)), headers=self.prepare_headers())
         self.cntr_id = response.json()['counters'][0]['id']
         return True
 
@@ -56,7 +57,7 @@ class YaCounters(YaBase):
         response = requests.get(self.COUNTERS_STAT_URL, params=req_params, headers=req_headers)
         return response.json()['totals'][0]
 
-yam = YaCounters('ID','TOKEN')
+yam = YaCounters('ID', 'TOKEN')
 yam_cntrs = yam.get_counter_id()
 yam_number = yam.get_visitors()
 print(yam_number)
